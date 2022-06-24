86420aa74352eb10a00dfe539e1c75c4de46f34f
Trung <vttrung12@gmail.com>
2022-06-23 21:46:54 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useRef } from 'react';
+
+// code mirror
+import CodeMirror from '@uiw/react-codemirror';
+import { python } from '@codemirror/lang-python';
+import { darcula } from '@uiw/codemirror-theme-darcula';
+import { javascript } from '@codemirror/lang-javascript';
+
+// antd
+import { Button } from 'antd';
+
 
 function App() {
+  // const codeValue = useRef(null);
+
+  const [code, setCode] = React.useState(
+    "console.log(\'hello world!\');\n\n\n"
+  );
+
+  const submitCode = (event) => {
+    console.log(event.target.codeValue.value)
+    // fetch('http://localhost:2358/submissions?wait=true', {
+    //   method: "POST",
+    //   headers: {
+    //     'Content-Type': 'application/json',
+    //   },
+    //   body: JSON.stringify({
+    //     source_code: code,
+    //     language_id: 63
+    //   })
+    // })
+    //   .then(response => response.json())
+    //   .then(data => {
+    //     console.log(data)
+    //   });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <CodeMirror
+        // value={code}
+        name="codeValue"
+        height="200px"
+        extensions={[javascript({ jsx: true })]}
+      />
+
+      <Button onClick={submitCode}>Submit Code</Button>
     </div>
+
   );
 }
 
